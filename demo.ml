45c37bff3a4a974c35ff8dd7f71d72acea8357d2
Guillaume Huysmans <ghuysmans99@gmail.com>
2020-12-25 15:58:39 +0100

    Compile in both directions

--- demo.ml
+++ demo.ml
@@ -1,3 +1,8 @@
 let () =
+  let f =
+    match Sys.argv with
+    | [| _; "-r" |] -> Monsql.en_fr
+    | _ -> Monsql.fr_en
+  in
   Lexing.from_channel stdin |>
-  Monsql.(compile fr_en) ~output:print_string
+  Monsql.compile f ~output:print_string
